4b71a4c48d84ee27efab19ef7cd6c7108c779a44
chandrakanthgunti <chandrakanth.gunti@gmail.com>
2022-02-23 10:02:48 +0530

    Implementation extended for other browsers

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -3,9 +3,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -13,26 +18,39 @@ import java.time.Duration;
 
 public class BrowserTest {
 
+
     @Test
-    public void launchBrowserTest(){
+    @Parameters({"browser"})
+    public void launchBrowserTest(String browser){
         System.out.println("@@@@@@@@@@@@@@@@@@ Test Browser Invocation @@@@@@@@@@@@@@@@@@@@@@@@");
             String OSName = System.getProperty("os.name").toLowerCase();
             System.out.println("@@@@@@@@@ Operating System is: "+OSName);
-            WebDriver driver;
+            WebDriver driver=null;
 
-            System.out.println("================================= Opening Browser ===========================");
+            System.out.println("================================= Opening Browser : "+browser+" ===========================");
 
-            //System.setProperty("webdriver.chrome.driver",driverPath);
-            //WebDriverManager.chromedriver().setup();
-
-            WebDriverManager.firefoxdriver().setup();
+            if(browser.equalsIgnoreCase("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions ffOptions = new FirefoxOptions();
                 //ffOptions.addArguments("-headless");
-            driver = new FirefoxDriver(ffOptions);
-            //ChromeOptions chOptions = new ChromeOptions();
-            //chOptions.addArguments("--headless");
-            //driver = new ChromeDriver(chOptions);
-
+                driver = new FirefoxDriver(ffOptions);
+            }else if(browser.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chOptions = new ChromeOptions();
+                //chOptions.addArguments("--headless");
+                driver = new ChromeDriver(chOptions);
+            }else if(browser.equalsIgnoreCase("internetexplorer")){
+                WebDriverManager.iedriver().setup();
+                InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+                //ieOptions.addCommandSwitches("--headless");
+                driver = new InternetExplorerDriver(ieOptions);
+            }else if(browser.equalsIgnoreCase("edge")){
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                //edgeOptions.addArguments("--headless");
+                driver = new EdgeDriver(edgeOptions);
+            }
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
             driver.manage().window().maximize();
             driver.get("http://google.com");
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
